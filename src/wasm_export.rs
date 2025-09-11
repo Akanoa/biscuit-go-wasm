575ce9816e9a6cf7ec7b4ad5652389feb8d18caa
Eloi DEMOLIS <eloi.demolis@clever-cloud.com>
2025-09-11 21:44:41 +0200

    fix: correctly type result in wasm_export

--- src/wasm_export.rs
+++ src/wasm_export.rs
@@ -1,13 +1,19 @@
 #[macro_export]
 macro_rules! wasm_export {
-    (fn $f:ident($($arg_name:ident: $arg_type:ty),* $(,)?) $(-> $ret:ty)? $body:block) => {
+    (fn $f:ident($($arg_name:ident: $arg_type:ty),* $(,)?) $(-> $ret_type:ty)? $body:block) => {
         #[unsafe(no_mangle)]
         pub fn $f(__ret: &mut WasmResult, $($arg_name: $arg_type),*) {
             #[allow(unused_mut)]
-            let mut ret = (|| $(-> $ret)? {
+            let mut ret = (|| -> $crate::wasm_export!(@ret_type $($ret_type)?) {
                 $body
             });
             __ret.capture(ret())
         }
+    };
+    (@ret_type) => {
+        ()
+    };
+    (@ret_type $ret_type:ty) => {
+        $ret_type
     };
 }
